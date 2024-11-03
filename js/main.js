733daaefe34c521dd49851e5a9c57e2a48348434
mangasarianbros <mangasarianbros@gmail.com>
2024-11-03 23:36:25 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-new HologramEffect();
+document.addEventListener('DOMContentLoaded', () => {
+    // Инициализираме всички ефекти
+    new QuantumParticle();
+    new HologramEffect();
+    
+    console.log('Квантовото просветление е заредено! 🌟');
+});
